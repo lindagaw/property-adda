9751d2bd894be3868a8b81d596aab193fd88e568
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-21 18:28:18 -0500

    WIP

--- translate.py
+++ translate.py
@@ -99,8 +99,6 @@ def train_step(images):
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
-    print(gradients_of_generator)
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
