9b9f5ee360f25c259780f7a21b7e6daa7d44f256
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-21 13:04:29 -0500

    WIP

---
+++ main.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataloader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyperparameters
+input_size = 20
