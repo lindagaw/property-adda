2fd32fa5aca6a643ddc284cf5c93c0607cf2e5d1
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-21 18:05:02 -0500

    WIP

--- translate.py
+++ translate.py
@@ -62,15 +62,13 @@ generator = make_generator_model()
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 def discriminator_loss(real_output, fake_output):
-    real_loss = tf.nn.softmax_cross_entropy_with_logits(tf.ones_like(real_output), real_output)
-    fake_loss = tf.nn.softmax_cross_entropy_with_logits(tf.zeros_like(fake_output), fake_output)
-
-    print(real_loss)
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
 
 def generator_loss(fake_output):
-    return tf.nn.softmax_cross_entropy_with_logits(tf.ones_like(fake_output), fake_output)
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
