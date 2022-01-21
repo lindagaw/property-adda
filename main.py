3f2c7e3720718fcd5ca76e0263c7254e88895505
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-21 14:20:06 -0500

    WIP

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+import tensorflow as tf
+from tf.keras.models import Sequential
+from tf.keras.layers import Dense
+from tf.keras.layers import LSTM
+from tf.keras.layers import Dropout
 
 def load_data(city_name):
     xs = np.load('data//pre-processed//'+ city_name + '_finalized_x.npy')
@@ -22,21 +23,14 @@ X_train = beijing_xs
 y_train = beijing_ys
 
 regressor = Sequential()
-
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
-
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
-
 regressor.add(Dense(units = 1))
-
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
