1e0206400c3324531f75a821cb3bd632dfc077f3
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-22 15:03:47 -0500

    WIP'

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0' #use GPU with ID=0
-config = tf..compat.v2.ConfigProto()
+config = tf.compat.v2.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5 # maximun alloc gpu50% of MEM
 config.gpu_options.allow_growth = True #allocate dynamically
 sess = tf.Session(config = config)
