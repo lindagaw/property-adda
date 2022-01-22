1cf66ac2aa1971f4d633bd0b3b9d265abd28ee88
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-22 13:35:16 -0500

    WIP

--- translate.py
+++ translate.py
@@ -136,6 +136,31 @@ def train_step(images):
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
+def train_step_no_mahalanobis(images):
+    noise = np.random.rand(BATCH, 1, 18)
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = tf.expand_dims(generator(noise, training=True), axis=1)
+
+        try:
+            real_output = discriminator(images, training=True)
+        except:
+            real_output = discriminator(np.asarray([images]), training=True)
+
+        fake_output = discriminator(generated_images, training=True)
+
+
+        #gen_loss = generator_loss(fake_output) + mahalanobis_loss(generated_images, mean, inv_covar)
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    print('generator loss: {}, discriminator_loss:{}'.format(gen_loss, disc_loss))
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
 def train(dataset, epochs):
   for epoch in range(epochs):
       print('training the {}th epoch'.format(epoch))
