fcb70d089f0c5d2b154ddac162bd97211c7f5b55
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-22 14:17:10 -0500

    WIP

--- translate.py
+++ translate.py
@@ -154,7 +154,7 @@ def train_step_no_mahalanobis(images):
         gen_loss = generator_loss(fake_output)
         disc_loss = discriminator_loss(real_output, fake_output)
 
-    print('generator loss: {}, discriminator_loss:{}'.format(gen_loss, disc_loss))
+    #print('generator loss: {}, discriminator_loss:{}'.format(gen_loss, disc_loss))
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
