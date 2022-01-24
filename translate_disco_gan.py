2bd5092112eccfa5064c1d0ca513c087e0b1a698
Ashley Gao <ashleygao@Ashleys-MacBook-Pro.local>
2022-01-24 12:51:47 -0500

    WIP

--- translate_disco_gan.py
+++ translate_disco_gan.py
@@ -144,12 +144,12 @@ except:
 # beijing to tianjin
 train(beijing_xs, tianjin_xs, 10)
 b_to_t = generator(tianjin_xs)
-np.save('.//translated//tianjin_to_beijing_cycle.npy', b_to_t)
+np.save('.//translated//tianjin_to_beijing_disco.npy', b_to_t)
 
 train(beijing_xs, shenzhen_xs, 10)
 b_to_s = generator(shenzhen_xs)
-np.save('.//translated//shenzhen_to_beijing_cycle.npy', b_to_s)
+np.save('.//translated//shenzhen_to_beijing_disco.npy', b_to_s)
 
 train(beijing_xs, guangzhou_xs, 10)
 b_to_g = generator(guangzhou_xs)
-np.save('.//translated//guangzhou_to_beijing_cycle.npy', b_to_g)
+np.save('.//translated//guangzhou_to_beijing_disco.npy', b_to_g)
