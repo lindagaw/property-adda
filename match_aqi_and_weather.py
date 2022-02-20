6f03a80a8c40c6f62c0697d878b3fa87bed96c18
Ashley Gao <gaoye0718@gmail.com>
2022-02-20 12:32:48 -0500

    WIP

--- match_aqi_and_weather.py
+++ match_aqi_and_weather.py
@@ -28,8 +28,8 @@ def combine_meteorology_and_airquality():
         header = next(csv_reader)
 
         for row in csv_reader:
-            print(row[:3])
-            if row[:3] == '001' or row[:3] == '004' or row[:3] == '006' or row[:3] == '009':
+
+            if row[0][:3] == '001' or row[0][:3] == '004' or row[0][:3] == '006' or row[0][:3] == '009':
                 for item in appropriate_list:
                     if int(item[0]) == int(row[0][2]) and \
                         str_to_datetime(item[1]) - str_to_datetime(row[1]) < timedelta(hours=2) and \
