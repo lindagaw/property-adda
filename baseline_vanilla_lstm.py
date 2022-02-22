3d062b6fe4d34ff6aff84477fcb2f80b0c7e17bb
Ashley Gao <gaoye0718@gmail.com>
2022-02-21 23:50:13 -0500

    WIP

--- baseline_vanilla_lstm.py
+++ baseline_vanilla_lstm.py
@@ -11,8 +11,7 @@ from tensorflow.keras.layers import Dropout
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 def load_air_quality_data(city):
     xs = np.load('data//pre-processed//' + city + '_pm25_xs.npy')
