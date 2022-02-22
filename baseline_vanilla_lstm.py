fa2168503dd8a2a9c4f569b528633f61a9f3ce20
Ashley Gao <gaoye0718@gmail.com>
2022-02-21 23:30:26 -0500

    WIP

---
+++ baseline_vanilla_lstm.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import pretty_errors
+import tensorflow as tf
+import sys
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def load_air_quality_data(city):
+    xs = np.load('data//pre-processed//' + city + '_pm25_xs.npy')
+    ys = np.load('data//pre-processed//' + city + '_pm25_ys.npy')
+
+    return xs, ys
+
+
+X, y = load_air_quality_data(sys.argv[1])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+regressor = Sequential()
+regressor.add(LSTM(units = 256, return_sequences = True, input_shape = (1, 18)))
+regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 1024, return_sequences = True))
+regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 2048, return_sequences = True))
+regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 1024, return_sequences = True))
+regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 256))
+regressor.add(Dropout(0.2))
+regressor.add(Dense(units = 1))
+regressor.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+, loss = 'mean_squared_error')
+regressor.fit(X_train, y_train, epochs = 150, batch_size = 32)
+
+y_pred = np.squeeze(regressor.predict(X_test))
+mse = mean_squared_error(y_test, y_pred, squared=True)
+print('the testing mse error is {}'.format(mse))
